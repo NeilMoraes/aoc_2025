8e536292dba88a4ae6ccced6f3356ff5cebf758f
Neil Moraes <mosraughen@gmail.com>
2025-12-02 08:18:47 +1300

    completed

--- day1.py
+++ day1.py
@@ -1,21 +1,22 @@
 from utils import import_data
 
 
-data = import_data("data/day1.txt")
-
-
 # Part 1
-def part1():
+def part1(data: list[str]):
+
+    # Variables
     value = 50
     counter = 0
 
     for line in data:
         direction, rotation = line[0], int(line[1:])
 
+        # Perform a rotation
         value = (
             (value + rotation) % 100 if direction == "R" else (value - rotation) % 100
         )
 
+        # Increment counter
         if value == 0:
             counter += 1
 
@@ -23,33 +24,41 @@ def part1():
 
 
 # Part 2
-def part2():
+def part2(data: list[str]):
+
+    # Variables
     value = 50
     counter = 0
 
     for line in data:
         direction, rotation = line[0], int(line[1:])
+
+        # Perform a rotation
         prev_value = value
+        overflow = (value + rotation) if direction == "R" else (value - rotation)
+        value = overflow % 100
 
-        delta = (value + rotation) if direction == "R" else (value - rotation)
-        value = delta % 100
+        # Increment counter based on overflows
+        if (direction == "R") & (overflow > 99):
+            counter += overflow // 100
+        elif (direction == "L") & (overflow < 1):
+            counter += 1 + (abs(overflow) // 100) - (1 if prev_value == 0 else 0)
 
-        if direction == "R":
-            counter += delta // 100
-        elif (direction == "L") & (delta < 0) & (prev_value > 0):
-            counter += -delta // 100 + 1
-        elif (value == 0) & (direction == "L"):
-            counter += -delta // 100 if rotation > 100 else 1
-        elif value == 0:
-            counter += 1
-        print(prev_value, line, delta, value, counter)
     print(f"Part 2: {counter}")
 
 
 if __name__ == "__main__":
+    sample = import_data("data/day1_sample.txt")
+    data = import_data("data/day1.txt")
+
+    # Part 1 - Sample
+    part1(sample)
 
     # Part 1 - 1177
-    # part1()
+    part1(data)
 
-    # Part 2 - Not implemented yet - 6180
-    part2()
+    # Part 2 - Sample
+    part2(sample)
+
+    # Part 2 - 6768
+    part2(data)
